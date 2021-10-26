30788486425f429ec14a9f6592ce7fc8d1131af5
Babler <gabrielbabler@yahoo.com.br>
2021-10-25 23:56:18 -0300

    add new event service

--- src/main/java/com/gbabler/netflux/service/MovieService.java
+++ src/main/java/com/gbabler/netflux/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.gbabler.netflux.service;
 
 import com.gbabler.netflux.domain.Movie;
+import com.gbabler.netflux.domain.MovieEvent;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -9,4 +10,6 @@ public interface MovieService {
     Mono<Movie> getMovieById(String id);
 
     Flux<Movie> getAllMovies();
+
+    Flux<MovieEvent> streamMovieEvents(String id);
 }
